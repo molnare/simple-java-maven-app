5c70e3b2a5a3b165a61d8946a73d86d394d28b00
csikiss <108928451+csikiss@users.noreply.github.com>
2023-07-18 17:24:10 +0300

    Update Example.java

--- src/main/java/Example.java
+++ src/main/java/Example.java
@@ -15,7 +15,7 @@ public class Example {
 
     @RequestMapping("/hello-world/date")
     String date() {
-        return new Date().toString();qq
+        return new Date().toString();qqqq
     }
 
     public static void main(String[] args) {
